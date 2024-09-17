2037673f140810fdfcafeddbd7e731f8553eb913
Austin Akerley <aakerley@Austins-Air.home>
2024-09-16 19:00:39 -0500

    Start of 4

--- python/NeetCode150/4_group_anagrams.py
+++ python/NeetCode150/4_group_anagrams.py
@@ -2,5 +2,9 @@ from collections import Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = {}
-        for str in strs:
-        
+        for s in strs:
+            s_sorted = "".join(sorted(s))
+            s_list = anagrams.get(s_sorted, []) # List to be stored as the value, based off the counter as the key
+            s_list.append(s)
+            anagrams.update({s_sorted:s_list})
+        return [anagram_list for anagram_list in anagrams.values()]
